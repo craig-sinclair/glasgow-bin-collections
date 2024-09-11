2015fd3796f0aab80c0687c04947f48edcbd1ca8
craig-sinclair <2766838S@student.gla.ac.uk>
2024-09-11 15:57:41 +0100

    updated tailwind and about page

--- bin_collection/tailwind.config.js
+++ bin_collection/tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './templates/**/*.html'
+    './templates/**/*.html',
+    './src/**/*.{html,js}',
+    './components/**/*.{html,js}',
   ],
   theme: {
     extend: {},
